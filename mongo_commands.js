63485374b79024a19bd6c7ea1b31914f3faa3b81
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-07-19 13:23:53 +0530

    Add files via upload

--- mongo_commands.js
+++ mongo_commands.js
@@ -1,3 +1,111 @@
+// Types of data
+// - Structured - Tabular format- excel / csv / tsv
+// - Unstructured - Images / audio / video / raw text
+// - Semi-Structured - XML / JSON
+
+// Types of NoSQL DB
+// - Key Value Pair Based
+//    - Data is stored in key and value pair...
+//    - it is popular for storage like hashtables
+//    - data could be in JSON / BSON
+//    - redis, dynamo DB, Riak
+// - Column Oriented
+//    - data is stored in columns and we have some column family for each row and that column family contains key value pairs
+//    - based on google big table
+//    - HBase and Cassandra
+// - Document Oriented
+//    - collection of documents
+//    - data is stored in key and value and here value could be another document
+//    - MongoDB
+// - Graph Based
+//    - Graph Based databases are inspired from graph data structure
+// --------------------------------------------------------
+// MongoDB
+// - document oriented NoSQL database
+// - used for high volume data storage
+// - instead of tables we create collections that contains data in key and value pair
+// - collections contains set of documents
+// ---------------------------------------------------
+// MongoDB drivers are written on C++
+// MongoDB enhance performance based on load of data
+// if the load increases by adding more nodes (computers) the performance can be retained
+// --------------------------------------------------
+// Databases
+// - show dbs;
+// - use dbname;
+//    - if database doesn't exist than it creates a new database and switch to that
+//    - if database already exists than it simply switches to that database
+
+// ---------------------------------------
+// Collections
+// - show collections;
+// - db.createCollection(name, options)
+//   options : 
+//   - capped - true / flase
+//   - max - 10 : only 10 documents are allowed
+//   - size - size in bytes
+//   - AutoIndexId - automatically creates index on ID field
+// -------------------------------------------------------
+// Data Types in MongoDB
+// - Integer
+// - Double
+// - String
+// - Boolean
+// - Arrays
+// - Object
+// - Binary
+// - Javascript
+// - Date and TimeStamp
+// ======================================
+// CRUD Operations - Create Read Update Delete
+// create - insert / insertOne / insertMany
+// read - find / findOne
+// update - updateOne / updateMany / replaceOne / findOneAndUpdate
+// delete - deleteOne / deleteMany / remove
+// --------------------------------------------------------------------
+// Query Selectors & Projection Operators
+
+// Query Selectors
+// - Comparison
+// - Array
+// - Logical
+// - Element
+// - Comments
+// --------------------------------------------------------------------
+// Projections Operators
+// $
+// $elemMatch
+// $meta
+// $slice
+// --------------------------------------------------------------------
+// Comparison Operators
+// - $eq
+// - $gt
+// - $lt
+// - $gte
+// - $lte
+// - $in
+// - $ne
+// - $nin
+// --------------------------------------------------------------------
+// Logical Operators
+// - $and
+// - $or
+// - $not
+// - $nor
+// --------------------------------------------------------------------
+// Element Operators
+// - $exists
+// - $type
+// --------------------------------------------------------------------
+// Evaluation Operators
+// - $expr
+// - $regex
+// - jsonSchema
+// - $where
+
+
+
 // check databases available
 show dbs;
 
@@ -171,4 +279,32 @@ db.faculty_details.insertMany([
     }
 ]);
 
+db.faculties.aggregate([
+    {
+        $lookup : {
+            from : "faculty_details",
+            localField : "email",
+            foreignField : "email_id",
+            as : "output"
+        }
+    }
+]);
 
+db.faculties.aggregate([
+    {
+        $lookup : {
+            from : "faculty_details",
+            localField : "email",
+            foreignField : "email_id",
+            as : "output"
+        }
+    },
+    {
+        $project : {
+            name : 1,
+            email : 1,
+            "output.subjects" : 1,
+            "output.year" : 1
+        }
+    }
+]);
